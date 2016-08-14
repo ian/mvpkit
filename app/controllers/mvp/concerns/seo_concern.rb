2c38d5e8ab3471603c34ae137c7da49f4e450b33
Ian Hunter <ianhunter@gmail.com>
2016-08-15 00:24:20 +0200

    move over / helpers

--- app/controllers/mvp/concerns/seo_concern.rb
+++ app/controllers/mvp/concerns/seo_concern.rb
@@ -5,7 +5,13 @@ module Mvp
     private
 
     included do
+      before_action :force_no_trailing_slash
       helper_method :page
+    end
+
+    def force_no_trailing_slash
+      return if request.path == '/'
+      redirect_to request.original_url.gsub(/\/$/,'') if request.original_url.match(/\/$/)
     end
 
     def page
