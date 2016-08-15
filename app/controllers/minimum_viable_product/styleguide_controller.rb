2bd3e79e4d2a21ae8e78497b70f87cff35c1648b
Ian Hunter <ianhunter@gmail.com>
2016-08-15 16:24:11 +0200

    fix styleguide

--- app/controllers/minimum_viable_product/styleguide_controller.rb
+++ app/controllers/minimum_viable_product/styleguide_controller.rb
@@ -2,16 +2,24 @@ require_dependency "minimum_viable_product/controller"
 
 module MinimumViableProduct
   class StyleguideController < Controller
+    def index
+      render layout: 'minimum_viable_product/basic'
+    end
+
+    def bootstrap
+      render layout: 'minimum_viable_product/basic'
+    end
+
     def layout_basic
-      render template: 'styleguide/layouts/basic', layout: 'basic'
+      render template: 'minimum_viable_product/styleguide/layouts/basic', layout: 'minimum_viable_product/basic'
     end
 
     def layout_cover
-      render template: 'styleguide/layouts/cover', layout: 'cover'
+      render template: 'minimum_viable_product/styleguide/layouts/cover', layout: 'minimum_viable_product/cover'
     end
 
     def layout_carousel
-      render template: 'styleguide/layouts/carousel', layout: 'carousel'
+      render template: 'minimum_viable_product/styleguide/layouts/carousel', layout: 'minimum_viable_product/carousel'
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,16 @@
-MinimumViableProduct::Engine.routes.draw do
+Rails.application.routes.draw do
   # mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
 
   if Rails.env.development?
-    get 'styleguide', to: 'styleguide#index'
-    get 'styleguide/bootstrap', to: 'styleguide#bootstrap'
+    get 'styleguide', to: 'minimum_viable_product/styleguide#index'
+    get 'styleguide/bootstrap', to: 'minimum_viable_product/styleguide#bootstrap'
     ['basic','cover','carousel'].each do |layout|
-      get "styleguide/layouts/#{layout}", to: "styleguide#layout_#{layout}"
+      get "styleguide/layouts/#{layout}", to: "minimum_viable_product/styleguide#layout_#{layout}"
     end
   end
 
-  get 'robots.txt', to: 'seo#robots', as: :robots
+  get 'robots.txt', to: 'minimum_viable_product/seo#robots', as: :robots
   get '/sitemap.xml.gz', to: redirect("https://#{ENV['AWS_S3_BUCKET']}.s3.amazonaws.com/nailsalons/sitemaps/sitemap.xml.gz"), as: :sitemap
 
-  get 'analytics/optout', to: 'sessions#optout'
+  get 'analytics/optout', to: 'minimum_viable_product/analytics#optout'
 end
