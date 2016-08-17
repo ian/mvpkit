5531c847534cdf0f25643d9eb62ceee8b50d5d97
Ian Hunter <ianhunter@gmail.com>
2016-08-17 15:15:16 +0200

    add ssl redirecter

--- config/initializers/ssl.rb
+++ config/initializers/ssl.rb
@@ -1,3 +1,12 @@
-if ENV['FORCE_SSL'].to_b == true
-  Rails.application.config.middleware.use Rack::SslEnforcer
+case ENV['FORCE_SSL']
+when /t|true|yes|y|on/
+  Rails.application.config.middleware.use SslEnforcerOnlyProtocol, :https_port => ENV['HOST_PORT']
 end
+
+BEGIN {
+  class SslEnforcerOnlyProtocol < Rack::SslEnforcer
+    def current_scheme
+      @request.scheme == 'https'
+    end
+  end
+}
