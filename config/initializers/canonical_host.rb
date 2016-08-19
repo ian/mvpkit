901b4fc034189a8cbb95b9d153636aac7c6bf81a
Ian Hunter <ianhunter@gmail.com>
2016-08-19 15:37:19 +0200

    dont use canonical, use HOST_DOMAIN

--- config/initializers/canonical_host.rb
+++ config/initializers/canonical_host.rb
@@ -1,3 +1,3 @@
-if ENV['CANONICAL_HOST']
-  Rails.application.config.middleware.use Rack::CanonicalHost, ENV['CANONICAL_HOST']
+if ENV['HOST_DOMAIN']
+  Rails.application.config.middleware.use Rack::CanonicalHost, ENV['HOST_DOMAIN']
 end
