21a48b124cea4d214003d08e6cf23b199adf4e9a
Ian Hunter <ianhunter@gmail.com>
2016-09-02 19:03:42 +0200

    fix slugification method

--- app/models/concerns/minimum_viable_product/slugification.rb
+++ app/models/concerns/minimum_viable_product/slugification.rb
@@ -5,7 +5,10 @@ module MinimumViableProduct
     module ClassMethods
       def slugify(name, opts={})
         _opts = opts.reverse_merge(:if => lambda{ |obj| obj.send("#{name}_changed?".to_sym) })
-        before_validation -> { self.slug = slugify(self.send(name.to_sym)) }, _opts
+        before_validation -> {
+          self.slug = slugify(self.send(name.to_sym))
+          self.slug += SecureRandom.uuid if self.class.find_by(slug: self.slug).present?
+        }, _opts
       end
     end
 
