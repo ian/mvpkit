d0be335d5f0ce90336e896255a1d6c345ea0995f
Ian Hunter <ianhunter@gmail.com>
2016-09-03 14:38:00 +0200

    remove this since it breaks es6 compilation with browserify

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,4 +1,3 @@
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
 Rails.application.config.browserify_rails.paths << Rails.root.join('node_modules')
-Rails.application.config.browserify_rails.paths << -> (p) { p.start_with?(MinimumViableProduct::Engine.root.join("app").to_s) }
 Rails.application.config.browserify_rails.paths << MinimumViableProduct::Engine.root.join('app')
