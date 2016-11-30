a915735f5d9a7dc4d71537700b4293aa9f270ee9
Ian Hunter <ianhunter@gmail.com>
2016-11-30 11:03:10 +1100

    store_location takes path

--- app/controllers/concerns/minimum_viable_product/session_concern.rb
+++ app/controllers/concerns/minimum_viable_product/session_concern.rb
@@ -24,18 +24,20 @@ module MinimumViableProduct
     end
 
     # http://blog.laaz.org/tech/2012/12/27/rails-redirect_back_or_default/
-    def store_location
-      session[:return_to] = if request.get?
-        request.url
-      else
-        request.referer
-      end
+    def store_location(path=nil)
+      session[:return_to] = case
+                            when !path.nil?
+                              path
+                            when request.get?
+                              request.url
+                            else
+                              request.referer
+                            end
     end
 
     # http://blog.laaz.org/tech/2012/12/27/rails-redirect_back_or_default/
-    def redirect_back_or_default(default = root_url)
+    def redirect_back_or_default(default=nil)
       redirect_to(session.delete(:return_to) || default || request.referer)
     end
     alias_method :redirect_to_back_or_default, :redirect_back_or_default
-  end
 end
