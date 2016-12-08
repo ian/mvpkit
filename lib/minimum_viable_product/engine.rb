98a971132cea8baa5e7a0ed69070e3fc724e0ec8
Ian Hunter <ianhunter@gmail.com>
2016-12-08 16:41:55 +1100

    dont autoload, i thin this is the reason for doubleloading

--- lib/minimum_viable_product/engine.rb
+++ lib/minimum_viable_product/engine.rb
@@ -17,7 +17,7 @@ require_relative './ext/string'
 module MinimumViableProduct
   class Engine < ::Rails::Engine
     isolate_namespace MinimumViableProduct
-    config.autoload_paths << "#{Rails.root}/app"
-    config.autoload_paths << "#{Rails.root}/lib"
+    # config.autoload_paths << "#{Rails.root}/app"
+    # config.autoload_paths << "#{Rails.root}/lib"
   end
 end
