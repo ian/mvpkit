6308e46f67b52c85a714cdb7dde7cb4a4b6ab42c
Ian Hunter <ianhunter@gmail.com>
2017-01-14 20:41:36 +1100

    update routing so we don't use styleguide anymore, update routes for sitemap to use rails project name

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,6 @@
 MinimumViableProduct::Engine.routes.draw do
-  if Rails.env.development?
-    get 'styleguide', to: 'styleguide#index'
-    get 'styleguide/bootstrap', to: 'styleguide#bootstrap'
-    ['basic','cover','carousel'].each do |layout|
-      get "styleguide/layouts/#{layout}", to: "styleguide#layout_#{layout}"
-    end
-  end
-
   get 'robots.txt', to: 'seo#robots', as: :robots
-  get '/sitemap.xml.gz', to: redirect("https://#{ENV['AWS_S3_BUCKET']}.s3.amazonaws.com/#{ENV['PROJECT_NAME']}/sitemaps/sitemap.xml.gz"), as: :sitemap
+  get '/sitemap.xml.gz', to: redirect("https://#{ENV['AWS_S3_BUCKET']}.s3.amazonaws.com/#{Rails.application.class.parent_name.underscore}/sitemaps/sitemap.xml.gz"), as: :sitemap
 
   get 'analytics/optout', to: 'analytics#optout'
 end
