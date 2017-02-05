1eb5e02c569a825cfe8105e6595bb776a170d410
Ian Hunter <ianhunter@gmail.com>
2017-02-04 18:19:42 -0800

    dont autoslug anytime the title changes

--- app/models/concerns/minimum_viable_product/slugification.rb
+++ app/models/concerns/minimum_viable_product/slugification.rb
@@ -4,11 +4,12 @@ module MinimumViableProduct
 
     module ClassMethods
       def slugify(name, opts={})
-        _opts = opts.reverse_merge(:if => lambda{ |obj| obj.send("#{name}_changed?".to_sym) })
         before_validation -> {
-          self.slug = slugify(self.send(name.to_sym))
-          self.slug += SecureRandom.uuid if self.class.find_by(slug: self.slug).present?
-        }, _opts
+          return true if self.slug.present?
+          self.slug =  slugify(self.send(name.to_sym))
+          self.slug += "-#{SecureRandom.uuid}" if self.class.find_by(slug: self.slug).present?
+        }, opts
+        validates :slug, uniqueness: true
       end
     end
 
