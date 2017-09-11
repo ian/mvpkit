5138f7b600b177d283a1c2b0a3013dc237658f36
Ian Hunter <ianhunter@gmail.com>
2017-09-10 19:28:05 -0700

    fix admin to have http auth call too

--- project/app/engines/admin/app/controllers/admin/application_controller.rb
+++ project/app/engines/admin/app/controllers/admin/application_controller.rb
@@ -1,5 +1,6 @@
 module Admin
   class ApplicationController < ::ApplicationController
     protect_from_forgery with: :exception
+    before_action :http_basic_auth if ENV['HTTP_BASIC_AUTH_USER']
   end
 end
